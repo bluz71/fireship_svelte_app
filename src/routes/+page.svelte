78f0ec4165e5cc6dc3815b7bfe43e94e7795e77c
Dennis B <bluz71@users.noreply.github.com>
2023-08-27 13:49:53 +1000

    Add Tailwind and DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-green-400">Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 <a href="/hello">Hello</a>
+<hr>
+<button class="btn bg-indigo-700 text-stone-100 hover:bg-indigo-500">Button</button>
